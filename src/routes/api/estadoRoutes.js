8f34d63b07880bd255e1b478e2cac507d5695deb
Shoy09 <jhoel.dioses@tecsup.edu.pe>
2025-08-18 17:41:47 -0500

    update-rutas-estado

--- src/routes/api/estadoRoutes.js
+++ src/routes/api/estadoRoutes.js
@@ -3,21 +3,18 @@ const router = express.Router();
 const estadoController = require('../../controllers/estadoController');
 const verificarToken = require('../../middleware/auth');
 
-// Rutas protegidas con el middleware verificarToken
 router.get('/', verificarToken, estadoController.getAllEstados);
+
+// ================== RUTAS SUBESTADOS ==================
+router.get('/con-subestados', verificarToken, estadoController.getAllEstadosWithSub);
+router.get('/:id/subestados', verificarToken, estadoController.getSubEstadosByEstadoId);
+router.post('/:id/subestados', verificarToken, estadoController.createSubEstado);
+router.put('/subestados/:id', verificarToken, estadoController.updateSubEstado);
+router.delete('/subestados/:id', verificarToken, estadoController.deleteSubEstado);
+
 router.get('/:id', verificarToken, estadoController.getEstadoById);
 router.post('/', verificarToken, estadoController.createEstado);
 router.put('/:id', verificarToken, estadoController.updateEstado);
 router.delete('/:id', verificarToken, estadoController.deleteEstado);
-
-// ================== RUTAS SUBESTADOS ==================
-router.get('/:id/subestados', verificarToken, estadoController.getSubEstadosByEstadoId);
-router.post('/:id/subestados', verificarToken, estadoController.createSubEstado);
-router.put('/subestados/:id', verificarToken, estadoController.updateSubEstado);
-
-// Eliminar un subestado por ID
-router.delete('/subestados/:id', verificarToken, estadoController.deleteSubEstado);
-// Obtener todos los estados con subestados incluidos
-router.get('/con-subestados', verificarToken, estadoController.getAllEstadosWithSub);
 
 module.exports = router;
