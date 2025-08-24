134a7d7327f08a4f006d894437c287a1b81e654e
Shoy09 <jhoel.dioses@tecsup.edu.pe>
2025-08-24 13:20:48 -0500

    prueba3

--- src/app.js
+++ src/app.js
@@ -9,21 +9,12 @@ const swaggerUI = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json'); 
 const verificarToken = require('./middleware/auth');
 
-// ✅ Configuración de CORS
-app.use(cors({
-  origin: ['http://localhost:4200', 'https://seminco-norcobre.netlify.app'], 
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
-
-// ✅ Aceptar preflight OPTIONS
-app.options('*', cors());
-
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/api', routes);
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 
 const port = 3000;
 app.listen(port, '0.0.0.0', () => {
